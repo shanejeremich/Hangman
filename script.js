4be76e0ab9f2111c73c29f0db22d2b0eb436a7f0
Shane Jeremich <shane.jeremich@gmail.com>
2020-12-16 05:30:30 -0700

    implemented letter press event handler, added showNotification if letter is already chosen

--- script.js
+++ script.js
@@ -38,4 +38,43 @@ function displayWord() {
   // console.log(innerWord);
 }
 
+// Update the wrong letters
+function updateWrongLettersEl() {
+  console.log("Update wrong");
+}
+
+// Show notification
+function showNotification() {
+  notification.classList.add("show");
+
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener("keydown", e => {
+  if (e.code >= "KeyA" && e.code <= "KeyZ") {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
 displayWord();
