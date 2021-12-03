535ea5d80f8a3278890aab0cb2039ad508da2571
Shane Jeremich <shane.jeremich@gmail.com>
2021-12-03 02:09:44 -0700

    updated path for file

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
 const url = `http://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-12-31`;
-const apiKey = process.env.REACT_APP_APIKey;
+const apiKey = process.env.APIKey;
 
 export { url, apiKey };
